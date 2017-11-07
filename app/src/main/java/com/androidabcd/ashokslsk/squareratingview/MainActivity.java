2b96fe20cc6f8c327182d59982f5ea6ea082d060
ashokslsk <ashokslsk@gmail.com>
2017-11-07 10:41:46 +0530

    Initial commit

---
+++ app/src/main/java/com/androidabcd/ashokslsk/squareratingview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.androidabcd.ashokslsk.squareratingview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
